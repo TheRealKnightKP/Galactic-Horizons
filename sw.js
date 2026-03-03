4367baf7b0553292235b193d4f42c7cac3b22048
TheRealKnightKP <francoandretbr@gmail.com>
2026-03-03 16:57:11 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,39 @@
+const CACHE = "galactic-horizons-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./data.js",
+  "./shop.js",
+  "./cheat.js",
+  "./game.js",
+  "./Starlight.png",
+  "./Nemesis.png",
+  "./Mustang.png",
+  "./Cutlass.png",
+  "./Freelancer.png",
+  "./Hornet.png",
+  "./Constellation.png",
+  "./Hammerhead.png",
+  "./StarlancerTAC.png",
+  "./Polaris.png",
+  "./Kraken.png",
+  "./Idris.png",
+  "./Meteor.png",
+  "./Gladius.png",
+  "./Corsair.png",
+  "./Merlin.png",
+  "./VanduulKingship.png"
+];
 
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
