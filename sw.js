2e4964a41fb98d5d8986111e88abbd29e06e88ed
TheRealKnightKP <francoandretbr@gmail.com>
2026-03-05 21:36:35 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "galactic-horizons-v1.4.5";
+const CACHE = "galactic-horizons-v1.4.6";
 const ASSETS = [
   "./",
   "./index.html",
@@ -79,4 +79,5 @@ Versions:
 -V1.4.3 - Forgot to change the number displayed on index.html so i had to make a new version. 
 -V1.4.4 - Added Shadow Comet boss wave 12, fixed shield faces, added Vengeance secret ship.
 -V1.4.5 - Fixed shields and other stuff 
+-V1.4.6 - Changed aim assist on mobile
 */
