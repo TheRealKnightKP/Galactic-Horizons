fd3ceea98e8ef514d8ee05f9b628b97cac949d8a
TheRealKnightKP <francoandretbr@gmail.com>
2026-03-08 11:32:32 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "galactic-horizons-v1.5.0";
+const CACHE = "galactic-horizons-v1.5.1";
 const ASSETS = [
   "./",
   "./index.html",
@@ -94,4 +94,5 @@ Versions:
 -V1.4.18 - Fixed missile cycle
 -V1.4.19 - Fixing missile cycle again
 -1.5.0 - Arena Evolution part 1 update: Extra waves to wave 30, Shadow Vengeance bossfight, Retribution, Dreadnaught hp phases, Anti wobble.
+-1.5.1 - Fixed Waves 20-30, Shadow Vengeance bossfight fixed.
 */
