a402eb19b9e6358aa3d05bacd61ff05163e43c32
TheRealKnightKP <francoandretbr@gmail.com>
2026-03-14 15:24:41 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "galactic-horizons-v1.6.18";
+const CACHE = "galactic-horizons-v1.6.19";
 const ASSETS = [
   "./index.html",
   "./style.css",
@@ -128,5 +128,6 @@ Versions:
 -V1.6.15 - Fixed screen resolution and rendering problems and improved background
 -V1.6.16 - Fixed error in game.js
 -V1.6.17 - Fixing?
--V1.6.18 fixig resolution
+-V1.6.18 - fixing resolution
+-V1.6.19 - Fixing money
 */
