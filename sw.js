80a7ff6d9a34dd9488b467f2a174a264e968afba
TheRealKnightKP <francoandretbr@gmail.com>
2026-03-19 11:03:10 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "galactic-horizons-v1.8.29";
+const CACHE = "galactic-horizons-v1.8.30";
 const ASSETS = [
   "./index.html",
   "./style.css",
@@ -198,5 +198,6 @@ Versions:
 -V1.8.26 - Combat mission fixes 2 electric bogaloo
 -V1.8.27 - Fixed some stuff and added station assets
 -V1.8.28 - Fixed exploration missions and added warden stations
--V1.8.29 - Fixed mobile loading for stations
+-V1.8.29 - Fixed mobile loading for stations (No, i didnt)
+-V1.8.30 - Fixed mobile loading for stations for real now
 */
